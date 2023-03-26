26570fcd89245e85eedf5068df51260bd1f497fa
Nathan <ndawit611@gmail.com>
2023-03-26 14:18:11 +0300

    Responsive Adjustments

--- src/app.js
+++ src/app.js
@@ -103,3 +103,12 @@ particlesJS("particles-js", {
   },
   retina_detect: true,
 });
+
+function myFunction() {
+  var x = document.getElementById("navlinks");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+  }
+}
